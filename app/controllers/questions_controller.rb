6bead60f83888e9a6bc0b6ea5738947c2b70964d
jeremy wilson <jeremyw9187@gmail.com>
2021-02-09 20:54:23 -0500

    working coach who responds

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,5 +6,13 @@ class QuestionsController < ApplicationController
 
   def answer
     # if else statment for the logic
+    @question = params[:question]
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question[-1] == '?'
+      @answer = 'Silly question, get back to work!'
+    else
+      @answer = "I don't care. get dressed and go to work!"
+    end
   end
 end
